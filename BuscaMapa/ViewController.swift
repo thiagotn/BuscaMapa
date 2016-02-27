2d2c48adb77daae206fff3adc3857b64f0c868bc
Usuário Convidado <Guest@l2304micro25.local>
2016-02-27 14:08:17 -0300

    Initial Commit

---
+++ BuscaMapa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuscaMapa
+//
+//  Created by Usuário Convidado on 27/02/16.
+//  Copyright © 2016 Thiago Nogueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
